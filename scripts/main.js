b6bb58d81c4adacdb3da52d8c57c9fdfc4fdd63a
carlos <choyossuarez@gmail.com>
2021-06-30 18:02:47 -0500

    studying input/click/onchange events

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,31 @@
 //Lógica para el funcionamiento de la calculadora de vidrio//
 
 
-function calculo() {
+// function calculo() {
 
+//     let alto = document.getElementById("alturaVidrio").value;
+//     let ancho = document.getElementById("anchoVidrio").value;
+//     let espesor = document.getElementById("espesorVidrio").value;
+
+//     densidadVidrio = 2500;
+//     areaVidrio = alto*ancho;
+//     volumenVidrio = alto*ancho*(espesor/1000);
+//     pesoVidrio = densidadVidrio*volumenVidrio;
+
+//     document.getElementById("pesoFinal").innerHTML = pesoVidrio;
+//     document.getElementById("areaFinal").innerHTML = areaVidrio;
+
+//     console.log("La altura es: " + alto);
+//     console.log("El ancho es: " + ancho);
+//     console.log("El espesor es: " + espesor);
+//     console.log("El área del vidrio es: " + areaVidrio);
+//     console.log("El volumen del vidrio es: " + volumenVidrio);
+//     console.log("El peso del vidrio es: " + pesoVidrio);
+
+// };
+
+document.getElementById("espesorVidrio").addEventListener("input", () => {
+    
     let alto = document.getElementById("alturaVidrio").value;
     let ancho = document.getElementById("anchoVidrio").value;
     let espesor = document.getElementById("espesorVidrio").value;
@@ -13,14 +36,38 @@ function calculo() {
     pesoVidrio = densidadVidrio*volumenVidrio;
 
     document.getElementById("pesoFinal").innerHTML = pesoVidrio;
-    document.getElementById("areaFinal").innerHTML = areaVidrio;
+    
 
-    console.log("La altura es: " + alto);
-    console.log("El ancho es: " + ancho);
-    console.log("El espesor es: " + espesor);
-    console.log("El área del vidrio es: " + areaVidrio);
-    console.log("El volumen del vidrio es: " + volumenVidrio);
-    console.log("El peso del vidrio es: " + pesoVidrio);
+ });
 
-};
+document.getElementById("anchoVidrio").addEventListener("input", () => {
+    
+    let alto = document.getElementById("alturaVidrio").value;
+    let ancho = document.getElementById("anchoVidrio").value;
+    let espesor = document.getElementById("espesorVidrio").value;
 
+    densidadVidrio = 2500;
+    areaVidrio = alto*ancho;
+    volumenVidrio = alto*ancho*(espesor/1000);
+    pesoVidrio = densidadVidrio*volumenVidrio;
+
+    document.getElementById("pesoFinal").innerHTML = pesoVidrio;
+   
+
+});
+
+document.getElementById("alturaVidrio").addEventListener("input", () => {
+    
+    let alto = document.getElementById("alturaVidrio").value;
+    let ancho = document.getElementById("anchoVidrio").value;
+    let espesor = document.getElementById("espesorVidrio").value;
+
+    densidadVidrio = 2500;
+    areaVidrio = alto*ancho;
+    volumenVidrio = alto*ancho*(espesor/1000);
+    pesoVidrio = densidadVidrio*volumenVidrio;
+
+    document.getElementById("pesoFinal").innerHTML = pesoVidrio;
+   
+
+});
